5255e008fa6e8d1d5a2d8d637c214cea75481b43
Shreyas Moudgalya <smoudgal@hawk.iit.edu>
2017-01-29 06:26:46 +0000

    stuff

--- ml.py
+++ ml.py
@@ -5,8 +5,12 @@ import statsmodels.formula.api as smf
 data = pd.read_csv('data.csv', index_col=0)
 data.head()
 
-fig, axs = plt.subplots(1, 1)
-data.plot(kind='scatter', x='Heart', y='BPM', ax=axs[0], figsize=(16, 8))
+#fig, axs = plt.subplots(1, 1)
+#data.plot(kind='scatter', x='Heart', y='BPM', ax=axs[0], figsize=(16, 8))
 
-lm = smf.ols(formula='Sales ~ TV', data=data).fit()
+
+df = DataFrame(rand(50, 4), columns=['a', 'b', 'c', 'd'])
+df.plot(kind='scatter', x='a', y='b');
+
+lm = smf.ols(formula='a ~ b', data=data).fit()
 lm.params
