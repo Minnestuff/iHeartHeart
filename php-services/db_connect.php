76f4771e963c53438b903855009e8e27c5966081
Amit Nandan P <amit_nndn@yahoo.co.in>
2017-02-06 00:03:44 -0600

    Removing db name from connect string

--- php-services/db_connect.php
+++ php-services/db_connect.php
@@ -1,7 +1,7 @@
 <?php
 function db_connect(){
 	$db_array = parse_ini_file("config.ini");
-	$servername = "wordpress-site.ce9wf7481i14.us-west-2.rds.amazonaws.com";
+	$servername = $db_array["db_server"];
 	$username = $db_array["db_user"];
 	$password = $db_array["db_password"];
 	$dbname = $db_array["db_name"];
